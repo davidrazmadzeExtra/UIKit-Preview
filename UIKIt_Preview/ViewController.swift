361aa35dac02b8358636721baade695dbfca004a
David Razmadze <razd45@gmail.com>
2022-11-12 10:17:57 -0500

    Initial Commit

---
+++ UIKIt_Preview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIt_Preview
+//
+//  Created by David Razmadze on 11/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
